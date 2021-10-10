036a1da75a7c860d5e40300be6006070e04ed66f
ntrupin <natrupin@gmail.com>
2021-10-10 13:36:53 -0400

    Initial Commit

---
+++ Shared/ishtarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ishtarApp.swift
+//  Shared
+//
+//  Created by Noah Trupin on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ishtarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
